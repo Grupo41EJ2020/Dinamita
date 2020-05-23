f7ed7519419089c8f8ccf0d3a2f8aba915493d33
Sanjuana Monserrat Ortiz Tovar <s.ortiz-03@hotmail.com>
2020-05-23 12:25:48 -0500

    editar y borrar temas

--- Controllers/TemaController.cs
+++ Controllers/TemaController.cs
@@ -63,8 +63,71 @@ namespace MVCLaboratorio.Controllers
             }
             else
             {
-                return View("ERROR");
+                return View("Error no es posible mostrar los datos");
             }
+
+        }
+        public ActionResult Edit(int id)
+        {
+
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdTema", id));
+            DataTable dttema = BaseHelper.ejecutarConsulta("sp_Tema_ConsultarPorID", CommandType.StoredProcedure, parametros);
+            Tema ttema = new Tema();
+            if (dttema.Rows.Count > 0)
+            {
+                ttema.IdTema = int.Parse(dttema.Rows[0]["IdTema"].ToString());
+                ttema.Nombre = dttema.Rows[0]["Nombre"].ToString();
+
+                return View(ttema);
+            }
+            else
+            {
+                return View("Error no Existe");
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Tema datos)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdTema", id));
+            parametros.Add(new SqlParameter("@Nombre", datos.Nombre));
+
+
+            BaseHelper.ejecutarConsulta("sp_Tema_Actualizar", CommandType.StoredProcedure, parametros);
+            return RedirectToAction("Index", "Tema");
+
+
+        }
+        public ActionResult Delete(int id)
+        {
+            
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdTema", id));
+            DataTable dttema = BaseHelper.ejecutarConsulta("sp_Tema_ConsultarPorID", CommandType.StoredProcedure, parametros);
+            Tema ttema = new Tema();
+            if (dttema.Rows.Count > 0)
+            {
+                ttema.IdTema = int.Parse(dttema.Rows[0]["IdTema"].ToString());
+                ttema.Nombre = dttema.Rows[0]["Nombre"].ToString();
+
+                return View(ttema);
+            }
+            else
+            {
+                return View("Error, intentalo de nuevo");
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection frm)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdTema", id));
+            BaseHelper.ejecutarConsulta("sp_Tema_Eliminar", CommandType.StoredProcedure, parametros);
+            return RedirectToAction("Index", "Tema");
+
 
         }
 
